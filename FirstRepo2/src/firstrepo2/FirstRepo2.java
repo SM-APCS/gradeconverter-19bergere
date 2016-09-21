894262bc2be521b2c4c1762fc9e60b21d099dfbb
emmettberger <emmettberger@25.73.193.237>
2016-09-21 07:52:14 -0500

    I am committed

--- FirstRepo2/src/firstrepo2/FirstRepo2.java
+++ FirstRepo2/src/firstrepo2/FirstRepo2.java
@@ -15,7 +15,28 @@ public class FirstRepo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("I Got It");
+        int grade = readInt("What is your grade? ");
+    
+    if(grade >= 90)
+    {
+        System.out.println("A");
+    }
+    else if(grade >= 80 && grade <= 89)
+    {
+        System.out.println("B");
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+        System.out.println("C");
+    }
+    else if(grade >= 60 && grade <= 69)
+    {
+        System.out.println("D");
+    }
+    else
+    {
+        System.out.println("F");
+    }
     }
     
 }
